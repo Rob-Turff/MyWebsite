888e83607fd64ce1b3db1ae89d351cc5eb64d229
Rob-Turff <robert_turff@yahoo.com>
2020-05-25 12:51:43 +0100

    Added social media links, fixed other links

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from .forms import PostForm
 from .models import Post
 
+def home(request):
+    return render(request, 'mainpage/home.html', {})
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
