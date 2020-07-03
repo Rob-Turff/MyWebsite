87c4f2f370fd5362dc982994416b32e2474cc48c
Rob-Turff <robert_turff@yahoo.com>
2020-07-03 19:46:45 +0100

    added page to collect and serve an ip address

--- management/urls.py
+++ management/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('ip', views.display_ip, name='display_ip'),
+]
--- management/views.py
+++ management/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from management.models import Management
+
+
+@login_required
+def display_ip(request):
+    management_obj = Management.objects.first()
+    if request.method == "POST":
+        ip = request.POST.get("ip")
+        if Management.objects.count() == 1:
+            management_obj.ip = ip
+        else:
+            management_obj = Management(ip=ip)
+        management_obj.save()
+        return HttpResponse("ip read " + str(ip))
+    else:
+        if Management.objects.count() == 1:
+            return HttpResponse(management_obj.ip)
+        else:
+            return HttpResponse("No stored ip address")
